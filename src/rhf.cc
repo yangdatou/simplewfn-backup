ded7f4b3a764901b05eb5aa5789e74c148682e4b
Junjie Yang <yangjunjie0320@gmail.com>
2022-10-10 21:56:42 -0700

    update

--- src/rhf.cc
+++ src/rhf.cc
@@ -9,9 +9,6 @@ const int MAX_ITER = 100;
 
 extern "C" void test_rhf(int nao, int nelec_alph, int nelec_beta, double *s1e, double *h1e, double *h2e) {
     std::cout << "Armadillo version: " << arma::arma_version::as_string() << std::endl;
-    std::cout << "nao = " << nao << std::endl;
-    std::cout << "nelec_alph = " << nelec_alph << std::endl;
-    std::cout << "nelec_beta = " << nelec_beta << std::endl;
 
     Int1eAO s1e_ao = build_int1e_ao(s1e, nao);
     Int1eAO h1e_ao = build_int1e_ao(h1e, nao);
