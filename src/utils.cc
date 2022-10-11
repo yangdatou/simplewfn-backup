83af3c5ed5a2fb5f26359ded9ae51fa71fc7e2e8
Junjie Yang <yangjunjie0320@gmail.com>
2022-10-10 21:49:17 -0700

    update

--- src/utils.cc
+++ src/utils.cc
@@ -1,7 +1,5 @@
 #include "utils.h"
 
-#define INDEX(mu, nu) mu > nu ? mu *(mu + 1) / 2 + nu : nu *(nu + 1) / 2 + mu
-
 void print_matrix(const arma::mat &mat, std::string title)
 {
     auto ncol = mat.n_cols;
@@ -74,85 +72,85 @@ double get_int2e_ao_element(const Int2eAO &int2e_ao, int mu, int nu, int lm, int
     return int2e_ao(munu, lmsg);
 }
 
-Int2eMO make_eri_mo(const Int2eAO &eri_ao, MOCoeff mo_coeff)
-{
-    int p, q, r, s;
-    int pq, rs, pqrs;
+// Int2eMO make_eri_mo(const Int2eAO &eri_ao, MOCoeff mo_coeff)
+// {
+//     int p, q, r, s;
+//     int pq, rs, pqrs;
 
-    int mu, nu, lm, sg;
-    int munu, lmsg;
+//     int mu, nu, lm, sg;
+//     int munu, lmsg;
 
-    int nao = mo_coeff.rows();
-    int nmo = mo_coeff.cols();
+//     int nao = mo_coeff.rows();
+//     int nmo = mo_coeff.cols();
 
-    int npair_ao = nao * (nao + 1) / 2;
-    int npair_mo = nmo * (nmo + 1) / 2;
-    int neri_mo = npair_mo * (npair_mo + 1) / 2;
+//     int npair_ao = nao * (nao + 1) / 2;
+//     int npair_mo = nmo * (nmo + 1) / 2;
+//     int neri_mo = npair_mo * (npair_mo + 1) / 2;
 
-    double eri_mu_nu_lm_sg;
+//     double eri_mu_nu_lm_sg;
 
-    Matrix tmp(npair_mo, npair_ao);
-    Int1eAO xx_ao(nao, nao);
-    Int1eMO xx_mo(nmo, nmo);
+//     Matrix tmp(npair_mo, npair_ao);
+//     Int1eAO xx_ao(nao, nao);
+//     Int1eMO xx_mo(nmo, nmo);
 
-    tmp << Matrix::Zero(npair_mo, npair_ao);
-    xx_ao << Int1eAO::Zero(nao, nao);
-    xx_mo << Int1eMO::Zero(nmo, nmo);
+//     tmp << Matrix::Zero(npair_mo, npair_ao);
+//     xx_ao << Int1eAO::Zero(nao, nao);
+//     xx_mo << Int1eMO::Zero(nmo, nmo);
 
-    for (mu = 0, munu = 0; mu < nao; mu++)
-    {
-        for (nu = 0; nu <= mu; nu++, munu++)
-        {
+//     for (mu = 0, munu = 0; mu < nao; mu++)
+//     {
+//         for (nu = 0; nu <= mu; nu++, munu++)
+//         {
 
-            for (lm = 0, lmsg = 0; lm < nao; lm++)
-            {
-                for (sg = 0; sg <= lm; sg++, lmsg++)
-                {
-                    eri_mu_nu_lm_sg = get_eri_ao_element(eri_ao, mu, nu, lm, sg);
-                    xx_ao(lm, sg) = xx_ao(sg, lm) = eri_mu_nu_lm_sg;
-                }
-            }
+//             for (lm = 0, lmsg = 0; lm < nao; lm++)
+//             {
+//                 for (sg = 0; sg <= lm; sg++, lmsg++)
+//                 {
+//                     eri_mu_nu_lm_sg = get_eri_ao_element(eri_ao, mu, nu, lm, sg);
+//                     xx_ao(lm, sg) = xx_ao(sg, lm) = eri_mu_nu_lm_sg;
+//                 }
+//             }
 
-            xx_mo = mo_coeff.transpose() * xx_ao * mo_coeff;
+//             xx_mo = mo_coeff.transpose() * xx_ao * mo_coeff;
 
-            for (p = 0, pq = 0; p < nmo; p++)
-            {
-                for (q = 0; q <= p; q++, pq++)
-                {
-                    tmp(pq, munu) = xx_mo(p, q);
-                }
-            }
-        }
-    }
+//             for (p = 0, pq = 0; p < nmo; p++)
+//             {
+//                 for (q = 0; q <= p; q++, pq++)
+//                 {
+//                     tmp(pq, munu) = xx_mo(p, q);
+//                 }
+//             }
+//         }
+//     }
 
-    Int2eMO int2e_mo(neri_mo);
-    int2e_mo << Int2eMO::Zero(neri_mo);
+//     Int2eMO int2e_mo(neri_mo);
+//     int2e_mo << Int2eMO::Zero(neri_mo);
 
-    for (p = 0, pq = 0; p < nmo; p++)
-    {
-        for (q = 0; q <= p; q++, pq++)
-        {
+//     for (p = 0, pq = 0; p < nmo; p++)
+//     {
+//         for (q = 0; q <= p; q++, pq++)
+//         {
 
-            for (lm = 0, lmsg = 0; lm < nao; lm++)
-            {
-                for (sg = 0; sg <= lm; sg++, lmsg++)
-                {
-                    xx_ao(lm, sg) = xx_ao(sg, lm) = tmp(pq, lmsg);
-                }
-            }
+//             for (lm = 0, lmsg = 0; lm < nao; lm++)
+//             {
+//                 for (sg = 0; sg <= lm; sg++, lmsg++)
+//                 {
+//                     xx_ao(lm, sg) = xx_ao(sg, lm) = tmp(pq, lmsg);
+//                 }
+//             }
 
-            xx_mo = mo_coeff.transpose() * xx_ao * mo_coeff;
+//             xx_mo = mo_coeff.transpose() * xx_ao * mo_coeff;
 
-            for (r = 0, rs = 0; r < nmo; r++)
-            {
-                for (s = 0; s <= r; s++, rs++)
-                {
-                    pqrs = INDEX(pq, rs);
-                    int2e_mo(pqrs) = xx_mo(r, s);
-                }
-            }
-        }
-    }
+//             for (r = 0, rs = 0; r < nmo; r++)
+//             {
+//                 for (s = 0; s <= r; s++, rs++)
+//                 {
+//                     pqrs = INDEX(pq, rs);
+//                     int2e_mo(pqrs) = xx_mo(r, s);
+//                 }
+//             }
+//         }
+//     }
 
-    return int2e_mo;
-}
+//     return int2e_mo;
+// }
