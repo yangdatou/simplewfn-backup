b727df2ebc3060d8b071b2233404e51ac4d090e2
Junjie Yang <yangjunjie0320@gmail.com>
2022-10-10 17:20:22 -0700

    update

--- test/test-h2o.py
+++ test/test-h2o.py
@@ -23,7 +23,7 @@ def test_h2o(basis='ccpvdz'):
     assert h1e.shape == (nao,nao)
     assert h2e.shape == (nao, nao, nao, nao)
 
-    simplewfn_rhf = numpy.ctypeslib.load_library("rhf", "../bin/")
+    simplewfn_rhf = numpy.ctypeslib.load_library("rhf", "./bin/")
 
     simplewfn_rhf.test_rhf(
         ctypes.c_int(nao), 
